10bf6b1721299a501adfce81e8606cc05150cd09
Josareta <josareta@gmail.com>
2023-10-13 13:06:26 +0200

    Añadir Ejercicio 13

--- src/ejercicio-13/index.js
+++ src/ejercicio-13/index.js
@@ -1,1 +1,13 @@
-// Escribe tu código aquí:
+// Manejo de errores en llamadas a APIs
+const axios = require('axios')
+
+async function obtenerDatosDeAPI() {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts/invalid')
+    console.log(response.data)
+  } catch (error) {
+    console.error('Error al obtener datos:', error.message)
+  }
+}
+
+obtenerDatosDeAPI()
