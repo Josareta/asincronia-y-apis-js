96121fba0f2902ea7d52fec33e172fbcf8f7e10a
Josareta <josareta@gmail.com>
2023-10-13 13:44:13 +0200

    Añadir Ejercicio 14

--- src/ejercicio-14/index.js
+++ src/ejercicio-14/index.js
@@ -1,1 +1,12 @@
-// Escribe tu código aquí:
+// Encadenamiento de Promises con manipulación de datos
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())
+  .then((data) => {
+    return data.map((post) => post.title.toUpperCase())
+  })
+  .then((titles) => {
+    console.log('Títulos en mayúsculas:', titles)
+  })
+  .catch((error) => {
+    console.error('Error:', error)
+  })
